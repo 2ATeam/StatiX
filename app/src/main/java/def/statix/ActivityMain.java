15787e1545ecba9e13fd3fcfafeb604d143d2130
VAlux <OLEX3000@yandex.ru>
2014-02-15 20:27:01 +0200

    Simple rendering demo and rendering framework class

--- app/src/main/java/def/statix/ActivityMain.java
+++ app/src/main/java/def/statix/ActivityMain.java
@@ -1,17 +1,22 @@
 package def.statix;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import def.statix.rendering.RenderingSurface;
 
 public class ActivityMain extends ActionBarActivity {
+
+    RenderingSurface surface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +30,21 @@ public class ActivityMain extends ActionBarActivity {
         }
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        surface.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        surface.resume();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
@@ -49,16 +65,36 @@ public class ActivityMain extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public class PlaceholderFragment extends Fragment implements View.OnTouchListener {
+        //test
+        private Bitmap balka;
 
         public PlaceholderFragment() {
+
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_activity_main, container, false);
-            return rootView;
+            //View rootView = inflater.inflate(R.layout.fragment_activity_main, container, false);
+            surface = new RenderingSurface(this.getActivity());
+
+            //test demo
+                balka = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+                surface.setBalka(balka);
+            //
+            surface.setOnTouchListener(this);
+            return surface;
+        }
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN ||
+               motionEvent.getAction() == MotionEvent.ACTION_MOVE){
+                //test
+                    surface.moveBalka(motionEvent.getX(), motionEvent.getY());
+            }
+            return true;
         }
     }
 
---
+++ app/src/main/java/def/statix/rendering/RenderingSurface.java
@@ -0,0 +1,73 @@
+package def.statix.rendering;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Lux on 15.02.14.
+ */
+public class RenderingSurface extends SurfaceView implements Runnable{
+
+    private Thread renderingThread;
+    private SurfaceHolder surfaceHolder;
+    private boolean isOK;
+    private Bitmap balka;
+    private float x, y;
+
+    //test
+    public void setBalka(Bitmap balka) {
+        this.balka = balka;
+        x = y = 0;
+    }
+
+    //test
+    public void moveBalka(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public RenderingSurface(Context context) {
+        super(context);
+        surfaceHolder = getHolder();
+    }
+
+    @Override
+    public void run() {
+        while (isOK) {
+            if (!surfaceHolder.getSurface().isValid()) {
+                continue;
+            }
+
+            Canvas canvas = surfaceHolder.lockCanvas();
+            assert canvas != null;
+            //test
+                canvas.drawARGB(255, 250, 150, 20);
+                canvas.drawBitmap(balka, x - (balka.getWidth() /  2), y - (balka.getHeight() / 2), null);
+            //
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void pause(){
+        isOK = false;
+        while (true) {
+            try {
+                renderingThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            break;
+        }
+        renderingThread = null;
+    }
+
+    public void resume(){
+        isOK = true;
+        renderingThread = new Thread(this);
+        renderingThread.start();
+    }
+}
