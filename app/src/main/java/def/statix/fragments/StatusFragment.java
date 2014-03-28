87aac9f5c5413d34aedabc3bc89608efeb36c806
Adya <adya_a@mail.ru>
2014-03-28 05:02:18 +0200

    More kashi

--- app/src/main/java/def/statix/fragments/StatusFragment.java
+++ app/src/main/java/def/statix/fragments/StatusFragment.java
@@ -12,16 +12,20 @@ import def.statix.ActivityMain;
 import def.statix.R;
 import def.statix.calculations.constructions.Construction;
 import def.statix.calculations.constructions.StaticProblemSolver;
+import def.statix.construction.ConstructionUnit;
+import def.statix.construction.Force;
+import def.statix.construction.Plank;
+import def.statix.construction.unittypes.BindingType;
+import def.statix.construction.unittypes.ForceType;
+import def.statix.rendering.SceneController;
 import def.statix.utils.ui.StatusManager;
 
 public class StatusFragment extends Fragment {
 
-    private Button bSolve;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.status_fragment, container, false);
-        bSolve = (Button) v.findViewById(R.id.bSolve);
+        Button bSolve = (Button) v.findViewById(R.id.bSolve);
         bSolve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,7 +39,62 @@ public class StatusFragment extends Fragment {
                     StatusManager.setError(getActivity().getString(R.string.msg_construction_not_ready));
             }
         });
+
+        Button bTest = (Button) v.findViewById(R.id.bTest);
+        bTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                test();
+            }
+        });
+
         StatusManager.addStatusView((TextView) v.findViewById(R.id.tvStatus));
         return v;
     }
+
+    private void test() {
+        Construction c = new Construction();
+        SceneController sceneController = ((ActivityMain) getActivity()).getSurfaceFragment().getScene();
+        Plank p1 = new Plank(2, 90);
+        Plank p2 = new Plank(3, 0);
+        p1.setPosition(200, 200);
+        p2.setPosition(100, 200);
+        c.addUnit(p1);
+        c.addUnit(p2);
+
+        sceneController.addForce(200, 200, ForceType.CONCENTRATED);
+        ConstructionUnit unit = (sceneController.getSelected());
+        unit.setAngle(-60f);
+        ((Force) unit).applyToPlank(p1);
+        ((Force) unit).setValue(6);
+        c.addUnit(unit);
+        sceneController.addForce(200, 150, ForceType.DISTRIBUTED);
+        unit = (sceneController.getSelected());
+        unit.setAngle(90);
+        ((Force) unit).applyToPlank(p1);
+        ((Force) unit).setValue(2);
+        c.addUnit(unit);
+
+        sceneController.addForce(150, 200, ForceType.MOMENT);
+        unit = (sceneController.getSelected());
+        unit.setAngle(180);
+        ((Force) unit).applyToPlank(p2);
+        ((Force) unit).setValue(7);
+        c.addUnit(unit);
+
+        sceneController.addBinding(200, 100, BindingType.FIXED);
+        unit = (sceneController.getSelected());
+        unit.setAngle(0f);
+        c.addUnit(unit);
+
+        sceneController.addBinding(100, 200, BindingType.MOVABLE);
+        unit = (sceneController.getSelected());
+        unit.setAngle(-30f);
+        c.addUnit(unit);
+
+        StaticProblemSolver solver = new StaticProblemSolver();
+        float[] s = solver.solve(c);
+        StatusManager.setSuccess(getString(R.string.msg_solution) + ": R = " + String.format("%.2f", s[0]) + "; X = " + String.format("%.2f", s[1]) + "; Y = " + String.format("%.2f", s[2]));
+    }
+
 }
